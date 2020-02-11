a756abacfeb5e230f32d611663cede50053b985c
Nathan Barber <nathanbarber6@gmail.com>
2020-02-11 16:55:18 -0500

    added mutate property / runner

--- agents/genetic/src/Organism.py
+++ agents/genetic/src/Organism.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 class Organism:
-    def __init__(self, num_states, num_actions, num_hidden_layers, num_hidden_units, mutate=0.7, parent_weights=None, parent_biases=None):
+    def __init__(self, num_states, num_actions, num_hidden_layers, num_hidden_units, noise_level=0.7, parent_weights=None, parent_biases=None):
         # shapes
         self.num_states = num_states
         self.num_actions = num_actions
@@ -10,7 +10,7 @@ class Organism:
         self.num_hidden_units = num_hidden_units
 
         # evolution
-        self.mutate = mutate
+        self.noise_level = noise_level
         self.parent_weights = parent_weights
         self.parent_biases = parent_biases
 
@@ -75,10 +75,40 @@ class Organism:
         # mutate parent matrix 
         pass
 
+    def reduce_noise(self, delta):
+        self.noise_level -= delta
+        return self
+
     def mutate(self):
         # get matricies from hidden layers
+        weights = []
+        biases = []
+        for i in range(len(self.hidden_layers)):
+            with tf.get_variable_scope("hidden{}".format(i)):
+                weights.append(tf.get_variable("kernel"))
+                biases.append(tf.get_variable("bias"))
         # mutate matricies
+        for i in range(len(self.num_hidden_layers)):
+            w_max = tf.reduce_max(tf.abs(weights[i]))
+            b_max = tf.reduce_max(tf.abs(biases[i]))
+            w_dev = w_max * self.noise_level
+            b_dev = b_max * self.noise_level
+            w_noise = tf.random_normal(shape=tf.shape(weights[i]), mean=0., stddev=w_dev)
+            b_noise = tf.random_normal(shape=tf.shape(biases[i]), mean=0., stddev=b_dev)
+            weights[i] = tf.add(w_noise, weights[i])
+            biases[i] = tf.add(b_noise, biases[i])
         # pass mutated matricies to new organism class instance
+        child = Organism(
+            self.num_states, 
+            self.num_actions, 
+            self.num_hidden_layers, 
+            self.num_hidden_units,
+            self.noise_level,
+            weights, 
+            biases)
         # return mutated organism
-        # lather rinse repeat
+        return child
+
+    def cross(self):
+        # organism gene crossover (TODO)
         pass
--- agents/genetic/src/Runner.py
+++ agents/genetic/src/Runner.py
@@ -2,5 +2,51 @@ import tensorflow as tf
 import numpy as np
 
 class Runner:
-    def __init__(self):
-        
+    def __init__(self, sess, env, organism, init_hidden_layers, init_layer_units, gaussian_noise, gen_size):
+        self.env = env
+        self.sess = sess
+        self.organism = organism
+        self.gen_size = gen_size
+
+        # env layout
+        self.num_states = self.env.num_states
+        self.num_actions = self.env.num_actions
+
+        # organism initial layout
+        self.layers = init_hidden_layers
+        self.units = init_layer_units
+        self.noise = gaussian_noise
+
+        # organisms
+        self.gen = None
+        self.last_gen_fittest = None
+
+        # fitness scores
+        self.gen_fitness = []
+        self.total_fitness_store = []
+
+    def run_gen(self):
+        self.env.reset()
+        self.gen_fitness = []
+        self.gen = []
+
+        # generate organisms
+        if self.last_gen_fittest is None:
+            for i in range(self.gen_size):
+                self.gen.append(
+                    organism(self.num_states, self.num_actions, self.layers, self.units, self.noise))
+        else:
+            for i in range(len(self.last_gen_fittest)):
+                for _ in range(10):
+                    self.gen.append(
+                        self.last_gen_fittest[i].mutate())
+        # run generation
+        # (TODO) create env capable of hosting multple agents
+
+
+        # record total score
+        self.total_fitness_store.append(np.sum(self.gen_fitness))
+
+        # pull out top ten percent
+        top = sort([ (x, i) for (i, x) in enumerate(self.gen_fitness) ], reverse=True)
+        self.last_gen_fittest = [ gen[i[1]] for i in top ]
